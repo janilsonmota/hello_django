c834da85b74e08849dad147a9f3f9318bd92c613
janilsonmota <janilsonmota@outlook.com>
2021-12-05 22:18:21 -0300

    Commit adicionado funções aritiméticas ao hello world.

---
+++ core/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+
+def hello(request, nome, idade):
+    return HttpResponse(f'<h1>Hello {nome} de {idade} anos. </h1>')
+
+
+def soma(request, num, number):
+    return HttpResponse(f'{num} + {number} = {num+number}')
+
+
+def subtracao(request, num, number):
+    return HttpResponse(f'{num} - {number} = {num-number}')
+
+
+def multiplicacao(request, num, number):
+    return HttpResponse(f'{num} * {number} = {num*number}')
+
+
+def divisao(request, num, number):
+    return HttpResponse(f'{num} / {number} = {num/number}')
